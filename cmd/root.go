14f337ec20ab855f8eff295a99147bf61b88fc17
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2026-02-22 14:27:16 +0530

    Initial commit

---
+++ cmd/root.go
@@ -0,0 +1,58 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/VA-ibh-AV/termi/internal/ai"
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "termi [prompt]",
+	Short: "AI-powered terminal command helper",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		if len(args) == 0 {
+			return cmd.Help()
+		}
+
+		prompt := args[0]
+		resp, err := ai.Generate(cmd.Context(), prompt)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(resp.Command)
+
+		return nil
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.termi.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ internal/ai/openai.go
@@ -0,0 +1,79 @@
+package ai
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"github.com/openai/openai-go/v3"
+	"github.com/openai/openai-go/v3/option"
+	"github.com/openai/openai-go/v3/responses"
+	"github.com/openai/openai-go/v3/shared"
+
+	"encoding/json"
+
+	"github.com/invopop/jsonschema"
+)
+
+func GenerateSchema[T any]() map[string]any {
+	reflector := jsonschema.Reflector{
+		AllowAdditionalProperties: false,
+		DoNotReference:            true,
+	}
+	var v T
+	schema := reflector.Reflect(v)
+
+	data, _ := json.Marshal(schema)
+	var result map[string]any
+	json.Unmarshal(data, &result)
+	return result
+}
+
+type CommandResponse struct {
+	Command string `json:"command" jsonschema_description:"The command to execute"`
+}
+
+var CommandResposneSchema = GenerateSchema[CommandResponse]()
+
+func Generate(ctx context.Context, prompt string) (*CommandResponse, error) {
+
+	openAIKey := os.Getenv("OPENAI_KEY")
+
+	if len(openAIKey) == 0 {
+		return nil, errors.New("SET OPENAI_KEY in env")
+	}
+
+	client := openai.NewClient(
+		option.WithAPIKey(os.Getenv("OPENAI_KEY")), // defaults to
+	)
+
+	resp, err := client.Responses.New(ctx, responses.ResponseNewParams{
+		Prompt: responses.ResponsePromptParam{
+			ID:      "pmpt_699a98bd8340819384b2569ab9895ee9075cdf724abc82da",
+			Version: openai.String("7"),
+		},
+		Reasoning: shared.ReasoningParam{Effort: openai.ReasoningEffortLow},
+		Input:     responses.ResponseNewParamsInputUnion{OfString: openai.String(prompt)},
+		Model:     openai.ChatModelGPT5Nano2025_08_07,
+		Text: responses.ResponseTextConfigParam{
+			Format: responses.ResponseFormatTextConfigParamOfJSONSchema(
+				"command_response",
+				CommandResposneSchema,
+			),
+			Verbosity: responses.ResponseTextConfigVerbosityLow,
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var commandResponse CommandResponse
+	err = json.Unmarshal([]byte(resp.OutputText()), &commandResponse)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &commandResponse, nil
+}
